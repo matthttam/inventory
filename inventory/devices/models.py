d7ac49beb5057e1cfd109952e34f8afd690e632e
Matt Henry <matthttam@gmail.com>
2022-06-26 20:15:00 -0500

    Several changes to fix some issues found during testing

--- inventory/devices/models.py
+++ inventory/devices/models.py
@@ -4,19 +4,44 @@ from locations.models import Room, Building
 # Create your models here.
 
 
+class DeviceStatus(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Device statuses"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class DeviceManufacturer(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class DeviceModel(models.Model):
+    name = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        "DeviceManufacturer", on_delete=models.CASCADE)
+
+
 class Device(models.Model):
     serial_number = models.CharField(max_length=255, unique=True)
     asset_id = models.CharField(max_length=255, unique=True)
     notes = models.CharField(max_length=255, blank=True)
-    status = models.ForeignKey("DeviceStatus", on_delete=models.PROTECT)
-    google_id = models.CharField(max_length=255, blank=True)
+    status = models.ForeignKey(DeviceStatus, on_delete=models.PROTECT)
+    google_id = models.CharField(
+        max_length=255, blank=True, null=True, unique=True, default=None)
     google_status = models.CharField(max_length=255, blank=True)
     google_organization_unit = models.CharField(max_length=255, blank=True)
     google_enrollment_time = models.DateTimeField(null=True, blank=True)
     google_last_policy_sync = models.DateTimeField(null=True, blank=True)
     google_location = models.CharField(max_length=255, blank=True)
-    google_most_recent_user = models.CharField(max_length=255, blank=True)
-    device_model = models.ForeignKey("DeviceModel", on_delete=models.PROTECT)
+    google_most_recent_user = models.CharField(
+        max_length=255, blank=True)
+    device_model = models.ForeignKey(DeviceModel, on_delete=models.PROTECT)
     building = models.ForeignKey(
         Building, on_delete=models.SET_NULL, null=True, blank=True)
     room = models.ForeignKey(
@@ -40,30 +65,7 @@ class Device(models.Model):
 
 class DeviceAccessory(models.Model):
     name = models.CharField(max_length=255)
-    device_model = models.ManyToManyField("DeviceModel")
-
-
-class DeviceModel(models.Model):
-    name = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(
-        "DeviceManufacturer", on_delete=models.CASCADE)
+    device_model = models.ManyToManyField(DeviceModel)
 
     def __str__(self):
         return f"{self.manufacturer} {self.name}"
-
-
-class DeviceManufacturer(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.name}"
-
-
-class DeviceStatus(models.Model):
-    name = models.CharField(max_length=255)
-
-    class Meta:
-        verbose_name_plural = "Device statuses"
-
-    def __str__(self):
-        return f"{self.name}"
