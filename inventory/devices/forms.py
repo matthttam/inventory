a0df54c5890b98c5004b0c7057aedc15b009e15b
Matt Henry <matthttam@gmail.com>
2022-06-26 21:29:37 -0500

    Formatting

--- inventory/devices/forms.py
+++ inventory/devices/forms.py
@@ -7,11 +7,18 @@ from locations.models import Building, Room
 class DeviceForm(ModelForm):
     device_model = ModelChoiceField(queryset=DeviceModel.objects.all())
     building = ModelChoiceField(
-        queryset=Building.objects.filter(active=True), required=False)
-    room = ModelChoiceField(
-        queryset=Room.objects.filter(active=True), required=False)
+        queryset=Building.objects.filter(active=True), required=False
+    )
+    room = ModelChoiceField(queryset=Room.objects.filter(active=True), required=False)
 
     class Meta:
         model = Device
-        fields = ['serial_number', 'asset_id',
-                  'device_model', 'status', 'building', 'room', 'notes']
+        fields = [
+            "serial_number",
+            "asset_id",
+            "device_model",
+            "status",
+            "building",
+            "room",
+            "notes",
+        ]
--- inventory/devices/models.py
+++ inventory/devices/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from locations.models import Room, Building
+
 # Create your models here.
 
 
@@ -23,8 +24,7 @@ class DeviceManufacturer(models.Model):
 
 class DeviceModel(models.Model):
     name = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(
-        "DeviceManufacturer", on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey("DeviceManufacturer", on_delete=models.CASCADE)
 
 
 class Device(models.Model):
@@ -33,25 +33,25 @@ class Device(models.Model):
     notes = models.CharField(max_length=255, blank=True)
     status = models.ForeignKey(DeviceStatus, on_delete=models.PROTECT)
     google_id = models.CharField(
-        max_length=255, blank=True, null=True, unique=True, default=None)
+        max_length=255, blank=True, null=True, unique=True, default=None
+    )
     google_status = models.CharField(max_length=255, blank=True)
     google_organization_unit = models.CharField(max_length=255, blank=True)
     google_enrollment_time = models.DateTimeField(null=True, blank=True)
     google_last_policy_sync = models.DateTimeField(null=True, blank=True)
     google_location = models.CharField(max_length=255, blank=True)
-    google_most_recent_user = models.CharField(
-        max_length=255, blank=True)
+    google_most_recent_user = models.CharField(max_length=255, blank=True)
     device_model = models.ForeignKey(DeviceModel, on_delete=models.PROTECT)
     building = models.ForeignKey(
-        Building, on_delete=models.SET_NULL, null=True, blank=True)
-    room = models.ForeignKey(
-        Room, on_delete=models.SET_NULL, null=True, blank=True)
+        Building, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.asset_id} ({self.serial_number}) - {self.device_model}"
 
     def get_absolute_url(self):
-        return reverse('devices:detail', kwargs={'pk': self.pk})
+        return reverse("devices:detail", kwargs={"pk": self.pk})
 
     def location(self):
         return self.room
