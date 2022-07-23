ae800c99ceb5df5f0ce3476f70496fdf5083311d
Matt Henry <matthttam@gmail.com>
2022-07-23 15:44:42 -0500

    Trying to test infobox but having issues with dates.

--- inventory/assignments/tests/test_templates/test_partials/infobox.py
+++ inventory/assignments/tests/test_templates/test_partials/infobox.py
@@ -13,8 +13,8 @@ default_context = Context(
             "id": 1,
             "person": "Test Person",
             "device": "Test Device",
-            "assignment_datetime": "",
-            "deviceassignment.return_datetime": "",
+            "assignment_datetime": "21/7/2022 8:57 PM",
+            "return_datetime": "",
         }
     }
 )
@@ -24,34 +24,53 @@ default_template = Template(
 )
 
 
-class DeviceAssignmentInfoBoxTest(SimpleTestCase):
+class DeviceAssignmentInfoboxTest(SimpleTestCase):
+    def setUp(self):
+        self.context = copy.deepcopy(default_context)
+        self.template = copy.deepcopy(default_template)
+
     def test_fields_exist(self):
+        expected_fields = [
+            {
+                "label": "Assignment ID :",
+                "value": self.context["deviceassignment"]["id"],
+            },
+            {
+                "label": "Person :",
+                "value": self.context["deviceassignment"]["person"],
+            },
+            {
+                "label": "Device :",
+                "value": self.context["deviceassignment"]["device"],
+            },
+            {
+                "label": "Assignment Date :",
+                "value": "07/21/2022 8:57 p.m.",
+            },
+            {
+                "label": "Return Date :",
+                "value": self.context["deviceassignment"]["return_datetime"],
+            },
+        ]
         context = copy.deepcopy(default_context)
         template = copy.deepcopy(default_template)
         rendered = template.render(context)
         soup = BeautifulSoup(rendered, "html.parser")
 
-        list_link = soup.select(list_link_selector)
-        update_link = soup.select(update_link_selector)
-        delete_link = soup.select(delete_link_selector)
+        info_divs = soup.select_one('p[name="assignment_card_body"]').find_all_next(
+            "div", class_="row"
+        )
 
-        self.assertEqual(len(list_link), 1)
-        self.assertEqual(len(update_link), 1)
-        self.assertEqual(len(delete_link), 1)
-
-        self.assertEqual(list_link[0].contents[0], "Assignment List")
-        self.assertEqual(update_link[0].contents[0], "Edit Assignment")
-        self.assertEqual(delete_link[0].contents[0], "Delete Assignment")
+        self.assertEqual(len(info_divs), len(expected_fields))
+        for index, info_div in enumerate(info_divs):
+            self.assertIn(str(expected_fields[index]["label"]), str(info_div))
+            self.assertIn(str(expected_fields[index]["value"]), str(info_div))
 
     def test_bottom_infbox_card(self):
         pass
 
 
-class DeviceAssignmentControlButtonsWithoutPermissionTest(SimpleTestCase):
-    def setUp(self):
-        self.context = copy.deepcopy(default_context)
-        self.template = copy.deepcopy(default_template)
-
+class DeviceAssignmentInfoboxTestold(SimpleTestCase):
     def test_view_link_missing(self):
         self.context["perms"]["assignments"]["view_deviceassignment"] = False
         rendered = self.template.render(self.context)
