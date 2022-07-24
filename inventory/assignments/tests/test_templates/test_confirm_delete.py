788d549b63916941462e68964a1553a8d3e4743c
Matt Henry <matthttam@gmail.com>
2022-07-23 20:17:23 -0500

    Fix testing for partials and confirm delete

---
+++ inventory/assignments/tests/test_templates/test_confirm_delete.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import reverse
+from authentication.tests.factories import SuperuserUserFactory, User, UserFactory
+from assignments.models import DeviceAssignment
+from assignments.tests.factories import DeviceAssignmentFactory
+from inventory.tests.helpers import get_permission
+from bs4 import BeautifulSoup
+
+
+class DeviceAssignmentConfirmDeleteTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        SuperuserUserFactory(username="my_superuser@example.com")
+        DeviceAssignmentFactory(id=1)
+
+    def setUp(self):
+        self.user = User.objects.get(username="my_superuser@example.com")
+        self.client.force_login(self.user)
+        self.response = self.client.get(reverse("assignments:delete", args=[1]))
+
+    def test_templates(self):
+        self.assertTemplateUsed(self.response, "dashboard/dashboard.html")
+        self.assertTemplateUsed(
+            self.response, "assignments/partials/deviceassignment_infobox.html"
+        )
+
+    def test_title(self):
+        self.assertInHTML(
+            "Inventory - Assignment Confirm Delete", self.response.content.decode()
+        )
+
+    def test_buttons(self):
+        print(self.response.content.decode())
+        soup = BeautifulSoup(self.response.content.decode(), "html.parser")
+        submit_buttons = soup.select('a[type="submit"]')
+        cancel_buttons = soup.select('a[href="/assignments/1/"]')
+        self.assertEqual(len(submit_buttons), 1)
+        self.assertEqual(len(cancel_buttons), 1)
+        self.assertEqual(submit_buttons[0].contents[0], "Delete Assignment 1")
+        self.assertEqual(cancel_buttons[0].contents[0], "Cancel")
--- inventory/assignments/tests/test_templates/test_partials/infobox.py
+++ inventory/assignments/tests/test_templates/test_partials/infobox.py
@@ -1,7 +1,12 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.template import Context, Template
 from bs4 import BeautifulSoup
 import copy
+from assignments.tests.factories import DeviceAssignmentFactory
+from django.urls import reverse
+from zoneinfo import ZoneInfo
+from authentication.tests.factories import SuperuserUserFactory
+from datetime import datetime
 
 list_link_selector = 'a[href="/assignments/"]'
 update_link_selector = 'a[href="/assignments/1/edit/"]'
@@ -9,13 +14,21 @@ delete_link_selector = 'a[href="/assignments/1/delete/"]'
 
 default_context = Context(
     {
+        "TIME_ZONE": "America/Chicago",
         "deviceassignment": {
             "id": 1,
             "person": "Test Person",
             "device": "Test Device",
-            "assignment_datetime": "21/7/2022 8:57 PM",
+            "assignment_datetime": datetime(2022, 7, 1, 2, 0, 0, 0),
             "return_datetime": "",
-        }
+        },
+        "perms": {
+            "assignments": {
+                "view_deviceassignment": True,
+                "delete_deviceassignment": True,
+                "change_deviceassignment": True,
+            }
+        },
     }
 )
 
@@ -24,12 +37,24 @@ default_template = Template(
 )
 
 
+class TestTest(TestCase):
+    def test_something(self):
+        self.user = SuperuserUserFactory(username="my_superuser@example.com")
+        self.client.force_login(self.user)
+        DeviceAssignmentFactory(id=1)
+        response = self.client.get(reverse("assignments:detail", args=[1]))
+        # print(response)
+        print(response.context)
+        # print(response.context["TIME_ZONE"])
+
+
 class DeviceAssignmentInfoboxTest(SimpleTestCase):
     def setUp(self):
         self.context = copy.deepcopy(default_context)
         self.template = copy.deepcopy(default_template)
 
     def test_fields_exist(self):
+        """Verify the infobox loads the assignment data expected"""
         expected_fields = [
             {
                 "label": "Assignment ID :",
@@ -45,11 +70,11 @@ class DeviceAssignmentInfoboxTest(SimpleTestCase):
             },
             {
                 "label": "Assignment Date :",
-                "value": "07/21/2022 8:57 p.m.",
+                "value": "07/01/2022 2 a.m.",
             },
             {
                 "label": "Return Date :",
-                "value": self.context["deviceassignment"]["return_datetime"],
+                "value": "",
             },
         ]
         context = copy.deepcopy(default_context)
@@ -66,41 +91,15 @@ class DeviceAssignmentInfoboxTest(SimpleTestCase):
             self.assertIn(str(expected_fields[index]["label"]), str(info_div))
             self.assertIn(str(expected_fields[index]["value"]), str(info_div))
 
-    def test_bottom_infbox_card(self):
-        pass
-
-
-class DeviceAssignmentInfoboxTestold(SimpleTestCase):
-    def test_view_link_missing(self):
-        self.context["perms"]["assignments"]["view_deviceassignment"] = False
-        rendered = self.template.render(self.context)
-        soup = BeautifulSoup(rendered, "html.parser")
-        list_link = soup.select(list_link_selector)
-        update_link = soup.select(update_link_selector)
-        delete_link = soup.select(delete_link_selector)
-        self.assertEqual(len(list_link), 0)
-        self.assertEqual(len(update_link), 1)
-        self.assertEqual(len(delete_link), 1)
-
-    def test_update_link_missing(self):
-        self.context["perms"]["assignments"]["change_deviceassignment"] = False
-        rendered = self.template.render(self.context)
-        soup = BeautifulSoup(rendered, "html.parser")
-        list_link = soup.select(list_link_selector)
-        update_link = soup.select(update_link_selector)
-        delete_link = soup.select(delete_link_selector)
-        print(self.context)
-        self.assertEqual(len(list_link), 1)
-        self.assertEqual(len(update_link), 0)
-        self.assertEqual(len(delete_link), 1)
-
-    def test_delete_link_missing(self):
-        self.context["perms"]["assignments"]["delete_deviceassignment"] = False
-        rendered = self.template.render(self.context)
-        soup = BeautifulSoup(rendered, "html.parser")
-        list_link = soup.select(list_link_selector)
-        update_link = soup.select(update_link_selector)
-        delete_link = soup.select(delete_link_selector)
-        self.assertEqual(len(list_link), 1)
-        self.assertEqual(len(update_link), 1)
-        self.assertEqual(len(delete_link), 0)
+    def test_bottom_infobox_card(self):
+        """Verify providing a template for bottom_infobox_card loads that template"""
+        context = copy.deepcopy(default_context)
+        template = Template(
+            "{% include 'assignments/partials/deviceassignment_infobox.html' with bottom_infobox_card='assignments/partials/deviceassignment_control_buttons.html' %}"
+        )
+        rendered = template.render(context)
+        bottom_infobox_card_template = Template(
+            "{% include 'assignments/partials/deviceassignment_control_buttons.html' %}"
+        )
+        bottom_infobox_card_rendered = bottom_infobox_card_template.render(context)
+        self.assertIn(bottom_infobox_card_rendered, rendered)
