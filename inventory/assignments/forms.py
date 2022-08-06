7a01f2495994eed0dabd225b49d992d584b508e9
Matt henry <matt.henry@owensboro.kyschools.us>
2022-08-06 06:52:02 -0500

    Remove commented code

--- inventory/assignments/forms.py
+++ inventory/assignments/forms.py
@@ -23,22 +23,6 @@ class DeviceAssignmentForm(ModelForm):
         fields = ["person", "device"]
 
 
-# class DeviceAssignmentTurninForm(ModelForm):
-#    class Meta:
-#        model = DeviceAssignment
-#        #fields = ('assignment_datetime',)
-#        fields = ['person','device','return_datetime']
-#    person = ModelChoiceField(queryset=Person.objects.all(), disabled=True)
-#    device = ModelChoiceField(queryset=Device.objects.all(), disabled=True)
-#    #return_datetime = DateField(widget=TextInput(attrs={'readonly':'readonly'}))
-#    #widgets={'assignment_datetime': CharField(disabled=True)}
-#    #return_date = DateField(disabled=True)
-
-# class DeviceAssignmentTurninForm(Form):
-#    return_datetime = DateField(disabled=True)
-#    person = ModelChoiceField(queryset=Person.objects.all(), disabled=False)
-
-
 class DeviceAssignmentTurninForm(ModelForm):
     class Meta:
         fields = ["id"]
