e591fb7f2e3a96cd1f0adead628ff99f97fbb40b
Matt Henry <matthttam@gmail.com>
2022-08-14 17:41:44 -0500

    Tested that the device search box is focused on initial page load!

--- inventory/assignments/tests/test_templates/test_quick_assign.py
+++ inventory/assignments/tests/test_templates/test_quick_assign.py
@@ -1,13 +1,16 @@
+from assignments.tests.factories import DeviceAssignmentFactory
+from authentication.tests.factories import SuperuserUserFactory, User
+from bs4 import BeautifulSoup
 from django.test import TestCase
 from django.urls import reverse
+from inventory.tests.helpers import chrome_set_value, get_chrome_driver
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from seleniumlogin import force_login
+from selenium.webdriver.common.by import By
 
-from bs4 import BeautifulSoup
 
-from authentication.tests.factories import SuperuserUserFactory, User
-from assignments.tests.factories import DeviceAssignmentFactory
-
-
-class DeviceAssignmentQuickAssignFormTest(TestCase):
+class DeviceAssignmentQuickAssignTemplateTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         SuperuserUserFactory(username="my_superuser@example.com")
@@ -29,3 +32,26 @@ class DeviceAssignmentQuickAssignFormTest(TestCase):
         submit_buttons = soup.find("form").select('button[type="submit"]')
         self.assertEqual(len(submit_buttons), 1)
         self.assertInHTML(submit_buttons[0].contents[0], "Submit")
+
+
+class DeviceAssignmentQuickAssignLiveTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = get_chrome_driver()
+        superuser = SuperuserUserFactory()
+        force_login(superuser, self.browser, f"{self.live_server_url}/")
+
+    def test_quickassign_person_is_focused_load(self):
+        self.browser.get(f"{self.live_server_url}/assignments/quickassign/")
+        # person_searchbox = locate_with
+        person_search_box = self.browser.find_element(
+            by=By.XPATH,
+            value='//*[@id="check_form"]/div[1]/div/span/span/span[1]/input',
+        )
+        self.assertEqual(
+            person_search_box,
+            self.browser.switch_to.active_element,
+            msg="Device search box is not focused automatically!",
+        )
+
+        # print(dir(self.browser))
+        # self.assertEqual(self.browser.switchTo())
