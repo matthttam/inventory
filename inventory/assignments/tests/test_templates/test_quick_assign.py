7f9bcc5d9b87d76086caf8e82ae3d6fb524cade9
Matt Henry <matthttam@gmail.com>
2022-08-14 22:36:04 -0500

    stuff

--- inventory/assignments/tests/test_templates/test_quick_assign.py
+++ inventory/assignments/tests/test_templates/test_quick_assign.py
@@ -1,13 +1,15 @@
 from assignments.tests.factories import DeviceAssignmentFactory
 from authentication.tests.factories import SuperuserUserFactory, User
 from bs4 import BeautifulSoup
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase, TestCase
 from django.urls import reverse
 from inventory.tests.helpers import chrome_set_value, get_chrome_driver
-from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from seleniumlogin import force_login
+from people.tests.factories import PersonFactory
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from seleniumlogin import force_login
 
 
 class DeviceAssignmentQuickAssignTemplateTest(TestCase):
@@ -39,19 +41,55 @@ class DeviceAssignmentQuickAssignLiveTest(StaticLiveServerTestCase):
         self.browser = get_chrome_driver()
         superuser = SuperuserUserFactory()
         force_login(superuser, self.browser, f"{self.live_server_url}/")
-
-    def test_quickassign_person_is_focused_load(self):
         self.browser.get(f"{self.live_server_url}/assignments/quickassign/")
-        # person_searchbox = locate_with
-        person_search_box = self.browser.find_element(
-            by=By.XPATH,
-            value='//*[@id="check_form"]/div[1]/div/span/span/span[1]/input',
+
+        self.device_search_xpath = (
+            '//*[@id="check_form"]/div[1]/div/span/span/span[1]/input'
         )
+
+        self.person_search_xpath = (
+            '//*[@id="check_form"]/div[2]/div/span/span/span[1]/input'
+        )
+
+        self.device_search_box = self.browser.find_element(
+            by=By.XPATH,
+            value=self.device_search_xpath,
+        )
+
+        self.person_search_box = self.browser.find_element(
+            by=By.XPATH,
+            value=self.person_search_xpath,
+        )
+
+    def test_device_search_is_focused_load(self):
+
+        # person_searchbox = locate_with
         self.assertEqual(
-            person_search_box,
+            self.device_search_box,
             self.browser.switch_to.active_element,
             msg="Device search box is not focused automatically!",
         )
 
+    def test_device_search_uses_asset(self):
+        pass
+
+    def test_device_search_uses_serial(self):
+        pass
+
+    def test_person_search_uses_internal_id(self):
+        pass
+
+        PersonFactory.create_batch(lastname="smith")
+        chrome_set_value(self.browser, self.person_search_box, "smi")
+
+        WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.NAME, "person_search"))
+        )
+
+        search_results = self.browser.find_element(
+            by=By.XPATH,
+            value='//*[@id="select2-person-results"]',
+        )
+        print(search_results)
         # print(dir(self.browser))
         # self.assertEqual(self.browser.switchTo())
