ab1c5529a26cedbf806edb9e90c1d821b03c1e5f
Matt henry <matt.henry@owensboro.kyschools.us>
2022-09-20 17:04:56 -0500

    chg: wip: mostly fixed the new ui

--- inventory/devices/static/devices/js/device_detail.js
+++ inventory/devices/static/devices/js/device_detail.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     console.log('loaded')
-    $("#minimize-inner-nav").on("click", function(){
+    $("a[data-inv-toggle='collapse']").on("click", function(e){
+        console.log($(this))
         $(this).toggleClass('inventory-icon-rotate-180 inventory-icon-rotate-360', 1000)
-        $(".inner-nav-text").toggle()
+        $($(this).attr('data-inv-target')).animate({'width': 'toggle'})
     })
 })
