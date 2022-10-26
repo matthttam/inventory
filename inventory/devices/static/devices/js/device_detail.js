28d7797e0c7bd2402f2a6b7eb8c8607492f5ac6e
Matt henry <matt.henry@owensboro.kyschools.us>
2022-10-26 08:05:25 -0500

    Working on adding inner nav bar features and making the app more compatible with mobile devices

--- inventory/devices/static/devices/js/device_detail.js
+++ inventory/devices/static/devices/js/device_detail.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-    console.log('loaded')
+    //$($("a[data-inv-toggle='collapse']").attr('data-inv-target')).hide()
+    //$($("a[data-inv-toggle='collapse']").attr('data-inv-target')).addClass('inventory-inner-nav-text-hidden')
     $("a[data-inv-toggle='collapse']").on("click", function(e){
-        console.log($(this))
+        inventoryInnerNav = $('.inventory-inner-nav').first()
         $(this).toggleClass('inventory-icon-rotate-180 inventory-icon-rotate-360', 1000)
         $($(this).attr('data-inv-target')).animate({'width': 'toggle'})
+        inventoryInnerNav.toggleClass('inventory-sidenav-toggled');
+        value = inventoryInnerNav.hasClass('inventory-sidenav-toggled')
+        localStorage.setItem('inventory|sidenav-toggle', value);
+        document.cookie="inventory_sidenav_toggle="+value+"; path=/;expires="
     })
 })
