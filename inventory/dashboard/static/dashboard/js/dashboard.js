ac28ac2a218e8e392e532d0b55e81089fb2a3891
Matt henry <matt.henry@owensboro.kyschools.us>
2022-10-27 14:56:01 -0500

    Fxied tab scrolling. Fixed suddenly appearing sidenav bar when it was hidden and the window was resized. Fixed sidenav overlay not covering the entire screen on smaller screens.

--- inventory/dashboard/static/dashboard/js/dashboard.js
+++ inventory/dashboard/static/dashboard/js/dashboard.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
         localStorage.setItem('sb|sidenav-toggle', value);
         document.cookie="sb_sidenav_toggle="+value+"; path=/;expires="
     })
+
+    $(window).on('resize orientationChanged', function(e) {
+        var breakpoint = 994
+        var windowWidth = $(window).width();
+        sbSideNav = $('.sb-nav-fixed').first()
+        is_hidden = sbSideNav.hasClass('sb-sidenav-toggled')
+        if(windowWidth < breakpoint && !is_hidden){
+            $("#sidebarToggle").trigger('click')
+        }else if(windowWidth > breakpoint && is_hidden){
+            $("#sidebarToggle").trigger('click')
+        }
+    })
 })
--- inventory/devices/static/devices/js/device_detail.js
+++ inventory/devices/static/devices/js/device_detail.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-    
+    // Auto hide innernav based on size and orientation changes.
     $(window).on('resize orientationChanged', function(e) {
+        var breakpoint = 1300
         var windowWidth = $(window).width();
         inventoryInnerNav = $('.inventory-innernav').first()
-        is_toggled = inventoryInnerNav.hasClass('inventory-innernav-toggled')
-        if(windowWidth < 960 && !is_toggled){
+        is_hidden = inventoryInnerNav.hasClass('inventory-innernav-toggled')
+        if(windowWidth < breakpoint && !is_hidden){
             $("a[data-inv-toggle='collapse']").trigger('click')
-        }else if(windowWidth > 960 && is_toggled){
+        }else if(windowWidth > breakpoint && is_hidden){
             $("a[data-inv-toggle='collapse']").trigger('click')
         }
     })
@@ -26,12 +27,15 @@ $(document).ready(function(){
        tab = $("a[href='"+hash+"']")
        if(tab){
         tab.tab('show')
-        $(window).scrollTop(0)
        }
     }
+
+    // Try really hard to scroll to the top of a tab when it is hiding or after it is shown or when the tab is clicked
+    $('.inventory-innernav a[data-bs-toggle="tab"]').on('hide.bs.tab shown.bs.tab click', e => {
+        $(this).scrollTop(0)
+    })
+    // Set URL Fragment for clicked link.
     $('.inventory-innernav a[data-bs-toggle="tab"]').on('shown.bs.tab', event => {
-        console.log('shown.bs.tab')
-        $(window).scrollTop(0)
         window.location.hash = $(event.target).attr('href');
     })
 })
